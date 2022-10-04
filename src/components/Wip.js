a5c6a9cb0837e7d63b9b9594e1394f95eed1f03e
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2022-10-04 16:15:45 +0300

    ...

--- src/components/Wip.js
+++ src/components/Wip.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Wip() {
-  return <div>Wip</div>;
+  return (
+    <div className="wip-container">
+      <div className="wip">
+        <h2> Work in Progress </h2>
+        <p>This page will be added soon...</p>
+      </div>
+    </div>
+  );
 }
 
 export default Wip;
